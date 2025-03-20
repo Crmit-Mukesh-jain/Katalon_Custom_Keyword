e9ad15c2b8bacf1a9f8b6259abc8115d696445db
Crmit-Mukesh-jain <mukesh.jain@crmit.com>
2025-03-20 17:50:28 +0530

    few changes

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>20635b3f-d3dc-4812-bc0e-1a2f558d98cb</testSuiteGuid>
+   <testCaseLink>
+      <guid>1bf94837-121a-4fe1-b41f-2a8347d6c4b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Application/Test case using Custom Keyword</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>5ca63cb1-7d93-4a3c-9ba6-374579f536d3</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
